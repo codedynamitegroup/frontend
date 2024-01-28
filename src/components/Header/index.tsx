399e35fd34afa2bf74af98e54c3e5a664541765a
Dương Chí Thông <20127634@student.hcmus.edu.vn>
2024-01-28 23:09:04 +0700

    Hoàn thành header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,181 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Drawer from "@mui/material/Drawer";
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import CloseIcon from "@mui/icons-material/Close";
+import { styled, useTheme } from "@mui/material/styles";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
+import classes from "./styles.module.scss";
+import { useEffect } from "react";
+const pages = ["Khám phá", "Luyện tập", "Cuộc thi"];
+const auth = ["Đăng nhập", "Đăng ký"];
+
+function ResponsiveAppBar() {
+  const drawerWidth = 240;
+  const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })<{
+    open?: boolean;
+  }>(({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen
+      }),
+      marginLeft: 0
+    })
+  }));
+
+  interface AppBarProps extends MuiAppBarProps {
+    open?: boolean;
+  }
+
+  const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== "open"
+  })<AppBarProps>(({ theme, open }) => ({
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    ...(open && {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: `${drawerWidth}px`,
+      transition: theme.transitions.create(["margin", "width"], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen
+      })
+    })
+  }));
+
+  const DrawerHeader = styled("div")(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end"
+  }));
+
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 900) {
+        setOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <AppBar position='static' open={open}>
+      <Container maxWidth='xl'>
+        <Toolbar disableGutters>
+          <Box
+            sx={{
+              display: { xs: "flex", md: "none" },
+              ...(open && { display: "none" })
+            }}
+          >
+            <IconButton
+              size='large'
+              aria-label='account of current user'
+              aria-controls='menu-appbar'
+              aria-haspopup='true'
+              onClick={handleDrawerOpen}
+              color='inherit'
+            >
+              <MenuIcon />
+            </IconButton>
+          </Box>
+          <Typography variant='h6' noWrap component='a' className={classes.logo}>
+            6BrosT
+          </Typography>
+          <Box className={classes.navbarItem}>
+            {pages.map((page) => (
+              <Button key={page} className={classes.item}>
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box className={classes.navbarAuthItem}>
+            <Button className={classes.item}>Đăng nhập</Button>
+            <Button className={classes.item}>Đăng ký</Button>
+          </Box>
+        </Toolbar>
+        <Drawer
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: drawerWidth,
+              boxSizing: "border-box"
+            },
+            display: { xs: "flex", md: "none" }
+          }}
+          variant='persistent'
+          anchor='left'
+          open={open}
+        >
+          <DrawerHeader>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === "ltr" ? <CloseIcon /> : <ChevronRightIcon />}
+            </IconButton>
+          </DrawerHeader>
+          <List>
+            {pages.map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemText primary={<Typography align='center'>{text}</Typography>} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {auth.map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemText primary={<Typography align='center'>{text}</Typography>} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+      </Container>
+    </AppBar>
+  );
+}
+export default ResponsiveAppBar;
