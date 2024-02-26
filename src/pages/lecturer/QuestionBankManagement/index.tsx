27d1f804d0a98c356e700bd19690e2334d72a51f
krnthoang542 <81230977+simplord542@users.noreply.github.com>
2024-02-26 22:36:53 +0700

    general question bank

--- src/pages/lecturer/QuestionBankManagement/index.tsx
+++ src/pages/lecturer/QuestionBankManagement/index.tsx
@@ -1,10 +1,35 @@
-import { Box, Tab, TabProps, Button } from "@mui/material";
+import {
+  Box,
+  Tab,
+  TabProps,
+  Stack,
+  Grid,
+  Container,
+  Button as MButton,
+  Divider
+} from "@mui/material";
+
+import Typography from "@mui/joy/Typography";
 import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { styled } from "@mui/material/styles";
 import Heading1 from "components/text/Heading1";
+import Heading6 from "components/text/Heading6";
+import EditIcon from "@mui/icons-material/Edit";
+import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
+import {
+  DataGrid,
+  GridColDef,
+  GridRowModesModel,
+  GridActionsCellItem,
+  GridToolbarContainer
+} from "@mui/x-data-grid";
+import Button from "@mui/joy/Button";
+import SearchBar from "components/common/search/SearchBar";
+import { red, grey } from "@mui/material/colors";
 
 const CustomTab = styled((props: TabProps) => <Tab {...props} />)(({ theme }) => ({
   textTransform: "none",
@@ -13,12 +38,143 @@ const CustomTab = styled((props: TabProps) => <Tab {...props} />)(({ theme }) =>
   fontSize: theme.typography.pxToRem(20),
   marginRight: theme.spacing(1)
 }));
+const rows = [
+  {
+    id: 1,
+    category: "Học thuật toán",
+    createdAtBy: { name: "Nguyễn Quốc Tuấn", time: "02/12/2023 10:30AM" },
+    updatedAtBy: { name: "Dương Chí Thông", time: "05/12/2023 10:30PM" }
+  },
+  {
+    id: 2,
+    category: "Java",
+    createdAtBy: { name: "Nguyễn Quốc Tuấn", time: "02/12/2023 10:30AM" },
+    updatedAtBy: { name: "Dương Chí Thông", time: "05/12/2023 10:30PM" }
+  },
+  {
+    id: 3,
+    category: "Mảng 1 chiều",
+    createdAtBy: { name: "Nguyễn Quốc Tuấn", time: "02/12/2023 10:30AM" },
+    updatedAtBy: { name: "Dương Chí Thông", time: "05/12/2023 10:30PM" }
+  },
+  {
+    id: 4,
+    category: "Mảng 2 chiều",
+    createdAtBy: { name: "Nguyễn Quốc Tuấn", time: "02/12/2023 10:30AM" },
+    updatedAtBy: { name: "Dương Chí Thông", time: "05/12/2023 10:30PM" }
+  },
+  {
+    id: 5,
+    category: "Con trỏ",
+    createdAtBy: { name: "Nguyễn Quốc Tuấn", time: "02/12/2023 10:30AM" },
+    updatedAtBy: { name: "Dương Chí Thông", time: "05/12/2023 10:30PM" }
+  }
+];
+// function EditToolbar() {
+//   return (
+//     <GridToolbarContainer>
+//       <Box>
+//         <MButton color='primary' startIcon={<AddIcon />}>
+//           Thêm mới
+//         </MButton>
+//         <Divider />
+//       </Box>
+//     </GridToolbarContainer>
+//   );
+// }
+
 const QuestionBankManagement = () => {
+  const [rowModesModel, setRowModesModel] = useState<GridRowModesModel>({});
   const [value, setValue] = useState("1");
+  const [pageState, setPageState] = useState({
+    isLoading: false,
+    data: rows,
+    total: 0,
+    page: 1,
+    pageSize: 5
+  });
+  const columns: GridColDef[] = [
+    {
+      field: "id",
+      headerName: "STT",
+      sortable: false,
+      width: 20,
+      align: "center",
+      headerClassName: "qbm-class"
+    },
+    {
+      field: "category",
+      headerName: "Tên danh mục",
+      sortable: false,
+      flex: 3,
+      headerClassName: "qbm-class"
+    },
+    {
+      field: "created",
+      headerName: "Ngày tạo bởi",
+      sortable: false,
+      flex: 3,
+      renderCell: (params) => (
+        <div>
+          <div>{params.row.createdAtBy.name}</div>
+          <div>{params.row.createdAtBy.time}</div>
+        </div>
+      ),
+      headerClassName: "qbm-class"
+    },
+    {
+      field: "updated",
+      headerName: "Lần chỉnh sửa cuối bởi",
+      sortable: false,
+      flex: 3,
+      renderCell: (params) => (
+        <div>
+          <div>{params.row.updatedAtBy.name}</div>
+          <div>{params.row.updatedAtBy.time}</div>
+        </div>
+      ),
+      headerClassName: "qbm-class"
+    },
+    {
+      field: "operation",
+      headerName: "Tác vụ",
+      sortable: false,
+      flex: 1,
+      type: "actions",
+      cellClassName: "actions",
+      getActions: ({ id }) => {
+        return [
+          <GridActionsCellItem
+            icon={<EditIcon />}
+            label='Save'
+            sx={{
+              color: "primary.main"
+            }}
+            onClick={() => null}
+          />,
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            label='Cancel'
+            className='textPrimary'
+            onClick={() => null}
+            sx={{
+              color: red[500]
+            }}
+          />
+        ];
+      },
+      headerClassName: "qbm-class"
+    }
+  ];
+  useEffect(() => {
+    //fetch data
+  }, [pageState.page, pageState.pageSize]);
 
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
   };
+
+  const [assignmentTypes, setAssignmentTypes] = useState(["Tự luận", "Nộp tệp"]);
 
   return (
     <Box>
@@ -30,8 +186,47 @@ const QuestionBankManagement = () => {
           </TabList>
         </Box>
         <TabPanel value='1'>
-          <Heading1 fontWeight={"500"}>Ngân hàng câu hỏi chung</Heading1>
-          <Button>Thêm mới</Button>
+          <Container maxWidth='xl'>
+            <Stack spacing={2}>
+              <Typography level='h1'>Ngân hàng câu hỏi chung</Typography>
+
+              <Grid container spacing={1}>
+                <Grid item xs={12} md={2}>
+                  <Button
+                    size='lg'
+                    variant='outlined'
+                    sx={{ fontSize: "120%", display: "block" }}
+                    fullWidth
+                  >
+                    Thêm mới
+                  </Button>
+                </Grid>
+              </Grid>
+
+              <SearchBar onSearchClick={() => null} placeHolder='Tìm kiếm theo danh mục ...' />
+              <DataGrid
+                sx={{
+                  zIndex: 0,
+                  "& .MuiDataGrid-columnHeader": { backgroundColor: grey[100] }
+                }}
+                autoHeight
+                rows={pageState.data}
+                rowCount={pageState.total}
+                loading={pageState.isLoading}
+                paginationModel={{ page: pageState.page, pageSize: pageState.pageSize }}
+                onPaginationModelChange={(model, details) => {
+                  setPageState((old) => ({ ...old, page: model.page, pageSize: model.pageSize }));
+                }}
+                columns={columns}
+                pageSizeOptions={[5, 10, 30, 50]}
+                paginationMode='server'
+                disableColumnFilter
+                // slots={{
+                //   toolbar: EditToolbar
+                // }}
+              />
+            </Stack>
+          </Container>
         </TabPanel>
         <TabPanel value='2'>Item Two</TabPanel>
       </TabContext>
