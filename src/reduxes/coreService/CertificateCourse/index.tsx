230c7df6f73b40041a8ad5662ce17f6905e4544e
ndqkhanh20 <khanhndq2002@gmail.com>
2024-05-16 17:38:36 +0700

    [FE] - feat: integrate apis for certificate course screen

--- src/reduxes/coreService/CertificateCourse/index.tsx
+++ src/reduxes/coreService/CertificateCourse/index.tsx
@@ -13,33 +13,6 @@ const initState: InitialState = {
   certificateCourses: []
 };
 
-// export const fetchAllCertificateCourses = createAsyncThunk(
-//   "certificateCourses/fetchAll",
-//   async (
-//     data: {
-//       courseName: string;
-//       filterTopicIds: string[];
-//       isRegisteredFilter: IsRegisteredFilterEnum;
-//     },
-//     { rejectWithValue }
-//   ) => {
-//     try {
-//       return CertificateCourseService.getCertificateCourses({
-//         courseName: data.courseName,
-//         filterTopicIds: data.filterTopicIds,
-//         isRegisteredFilter: data.isRegisteredFilter
-//       });
-//     } catch (error: any) {
-//       console.error("Failed to fetch certificate courses", error);
-//       return rejectWithValue({
-//         code: error.response?.code || 503,
-//         status: error.response?.status || "Service Unavailable",
-//         message: error.response?.message || error.message
-//       });
-//     }
-//   }
-// );
-
 const certificateCourseSlice = createSlice({
   name: "certificateCourse",
   initialState: initState,
@@ -52,19 +25,6 @@ const certificateCourseSlice = createSlice({
       state.mostEnrolledCertificateCourses = action.payload.mostEnrolledCertificateCourses;
     }
   }
-  // extraReducers: (builder) => {
-  //   builder.addCase(fetchAllCertificateCourses.pending, (state) => {
-  //     state.isLoading = true;
-  //   });
-  //   builder.addCase(fetchAllCertificateCourses.fulfilled, (state, action) => {
-  //     state.isLoading = false;
-  //     state.certificateCourses = action.payload.certificateCourses;
-  //     state.mostEnrolledCertificateCourses = action.payload.mostEnrolledCertificateCourses;
-  //   });
-  //   builder.addCase(fetchAllCertificateCourses.rejected, (state) => {
-  //     state.isLoading = false;
-  //   });
-  // }
 });
 
 export const { setLoading, setCertificateCourses } = certificateCourseSlice.actions;
