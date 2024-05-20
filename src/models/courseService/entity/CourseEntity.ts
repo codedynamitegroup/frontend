c6b872d76e956e96b9f57817ef5ce3e843176128
Đặng Ngọc Tiến <20127641@student.hcmus.edu.vn>
2024-05-20 11:31:44 +0700

    hot fix course user

--- src/models/courseService/entity/CourseEntity.ts
+++ src/models/courseService/entity/CourseEntity.ts
@@ -1,6 +1,9 @@
+import { UserCourseEntity } from "./UserCourseEntity";
+
 export interface CourseEntity {
   id: string;
   courseIdMoodle: string;
+  teachers: UserCourseEntity[];
   name: string;
   courseType: string;
   visible: boolean;
