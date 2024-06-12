bff5740f0709c6e8efded2f818c2d42490d622d6
Dương Chí Thông <20127634@student.hcmus.edu.vn>
2024-06-13 00:08:17 +0700

    Fe/intergrateUpdateAssignment

---
+++ src/models/courseService/entity/update/UpdateAssignmentCommand.ts
@@ -0,0 +1,14 @@
+export interface UpdateAssignmentCommand {
+  title: string;
+  intro?: string;
+  activity?: string;
+  wordLimit?: string;
+  maxUploadFiles: string;
+  maxFileSize: string;
+  maxScore: number;
+  timeOpen: string;
+  timeClose: string;
+  type: string;
+  visible: boolean;
+  allowSubmitLate: boolean;
+}
