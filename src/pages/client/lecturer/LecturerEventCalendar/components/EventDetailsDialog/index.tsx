3567ecc564a8560c5eb3c08420d483ba87cf4fc3
ndqkhanh20 <khanhndq2002@gmail.com>
2024-06-29 16:55:23 +0700

    [FE] - feat: fix lecturer event calendar

--- src/pages/client/lecturer/LecturerEventCalendar/components/EventDetailsDialog/index.tsx
+++ src/pages/client/lecturer/LecturerEventCalendar/components/EventDetailsDialog/index.tsx
@@ -1,21 +1,23 @@
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import FormatAlignLeftIcon from "@mui/icons-material/FormatAlignLeft";
+import SchoolIcon from "@mui/icons-material/School";
+import JoyButton from "@mui/joy/Button";
 import { DialogActions, DialogProps, Grid, Link } from "@mui/material";
 import Box from "@mui/material/Box";
 import CustomDialog from "components/common/dialogs/CustomDialog";
+import ParagraphBody from "components/text/ParagraphBody";
 import useWindowDimensions from "hooks/useWindowDimensions";
-import { useTranslation } from "react-i18next";
-import { IFullCalendarEvent } from "../..";
-import { NotificationEventTypeEnum } from "models/courseService/enum/NotificationEventTypeEnum";
-import AccessTimeIcon from "@mui/icons-material/AccessTime";
-import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
-import { standardlizeUTCStringToLocaleString } from "utils/moment";
 import i18next from "i18next";
+import { NotificationEventTypeEnum } from "models/courseService/enum/NotificationEventTypeEnum";
 import { useEffect, useState } from "react";
-import FormatAlignLeftIcon from "@mui/icons-material/FormatAlignLeft";
-import SchoolIcon from "@mui/icons-material/School";
-import { useNavigate } from "react-router-dom";
-import JoyButton from "@mui/joy/Button";
-import { routes } from "routes/routes";
+import { useTranslation } from "react-i18next";
 import ReactQuill from "react-quill";
+import { useNavigate } from "react-router-dom";
+import { routes } from "routes/routes";
+import { standardlizeUTCStringToLocaleString } from "utils/moment";
+import { IFullCalendarEvent } from "../..";
+import "./styles.module.scss";
 
 interface EventDetailsDialogProps extends DialogProps {
   data: IFullCalendarEvent;
@@ -60,7 +62,10 @@ const EventDetailsDialog = ({
       title={data.title}
       maxHeight={`${0.6 * height}px`}
       titleBackground={data.eventType === NotificationEventTypeEnum.COURSE ? "#FFD3BD" : "#DCE7EB"}
-      actionsDisabled={!data.editable && !data.exam && !data.assignment}
+      actionsDisabled={
+        !data.editable ||
+        (data.editable === true && (data.exam !== undefined || data.assignment !== undefined))
+      }
       confirmText='Edit'
       cancelText='Delete'
       onHandleCancel={onHandleCancel}
@@ -68,7 +73,7 @@ const EventDetailsDialog = ({
         if (openEditEventDialog) openEditEventDialog(data);
       }}
       customActions={
-        (data.exam || data.assignment) && (
+        (data.exam !== undefined || data.assignment !== undefined) && (
           <DialogActions>
             <JoyButton
               onClick={() => {}}
@@ -108,7 +113,14 @@ const EventDetailsDialog = ({
             />
           </Grid>
           <Grid item xs={12} sm={11}>
-            {standardlizeUTCStringToLocaleString(data.start, currentLang)}
+            <ParagraphBody>
+              {data.end
+                ? `${standardlizeUTCStringToLocaleString(data.start, currentLang)} » ${standardlizeUTCStringToLocaleString(
+                    data.end,
+                    currentLang
+                  )}`
+                : `${standardlizeUTCStringToLocaleString(data.start, currentLang)}`}
+            </ParagraphBody>
           </Grid>
         </Grid>
 
@@ -131,7 +143,11 @@ const EventDetailsDialog = ({
             />
           </Grid>
           <Grid item xs={12} sm={11}>
-            {data.title}
+            <ParagraphBody>
+              {data.eventType === NotificationEventTypeEnum.COURSE
+                ? t("calendar_event_course_event")
+                : t("calendar_event_user_event")}
+            </ParagraphBody>
           </Grid>
         </Grid>
 
@@ -155,7 +171,14 @@ const EventDetailsDialog = ({
               />
             </Grid>
             <Grid item xs={12} sm={11}>
-              <ReactQuill value={data.description || ""} readOnly={true} theme={"bubble"} />
+              <ReactQuill
+                value={data.description || ""}
+                readOnly={true}
+                theme={"bubble"}
+                style={{
+                  margin: "-12px -15px"
+                }}
+              />
             </Grid>
           </Grid>
         )}
@@ -196,7 +219,20 @@ const EventDetailsDialog = ({
                     );
                 }}
               >
-                {data.course.name || ""}
+                <ParagraphBody
+                  sx={{
+                    whiteSpace: "nowrap",
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                    color: "var(--blue-3)",
+                    "&:hover": {
+                      textDecoration: "underline",
+                      cursor: "pointer"
+                    }
+                  }}
+                >
+                  {data.course.name || ""}
+                </ParagraphBody>
               </Link>
             </Grid>
           </Grid>
