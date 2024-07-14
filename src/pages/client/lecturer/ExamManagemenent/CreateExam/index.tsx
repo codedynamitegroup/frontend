3d0368c9d058e37d44f4c5da7c14a6e609791e08
Đặng Ngọc Tiến <20127641@student.hcmus.edu.vn>
2024-07-14 23:51:34 +0700

    page question (temp)

--- src/pages/client/lecturer/ExamManagemenent/CreateExam/index.tsx
+++ src/pages/client/lecturer/ExamManagemenent/CreateExam/index.tsx
@@ -324,9 +324,9 @@ export default function ExamCreated() {
     setLoading(true);
     const formSubmitData: FormData = { ...data };
 
-    const questionIds = questionCreate.questionCreate.map((item) => ({
+    const questionIds = questionCreate.questionCreate.map((item, index) => ({
       questionId: item.id,
-      page: 0
+      page: index
     }));
 
     const timeLimitUnit = formSubmitData.timeLimit;
