ea4159a1f0c7c26843bdb45e43c295a6a6ba3da8
NguyenQuocTuan385 <nguyenquoctuan385@gmail.com>
2024-07-14 22:38:38 +0700

    [FE] - update section UI in course details

--- src/pages/client/lecturer/CourseManagement/Details/components/Assignment/index.tsx
+++ src/pages/client/lecturer/CourseManagement/Details/components/Assignment/index.tsx
@@ -1,4 +1,4 @@
-import { CircularProgress, Grid } from "@mui/material";
+import { CircularProgress, Collapse, Grid, IconButton, TextField, Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 import { BtnType } from "components/common/buttons/Button";
 import MenuPopup from "components/common/menu/MenuPopup";
@@ -10,90 +10,67 @@ import { routes } from "routes/routes";
 import AssignmentResource, { ResourceType } from "./components/Resource";
 import classes from "./styles.module.scss";
 import { useEffect, useMemo, useState, useCallback } from "react";
-import ReusedCourseResourceDialog from "./components/ReuseResourceDialog/CourseDialog";
-import ReusedResourceDialog from "./components/ReuseResourceDialog/ResourceDialog";
 import { useTranslation } from "react-i18next";
 import i18next from "i18next";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "store";
 import { ExamService } from "services/courseService/ExamService";
-import { setExamList, setExams, setLoadingExams } from "reduxes/courseService/exam";
 import { AssignmentService } from "services/courseService/AssignmentService";
-import { setAssignments, setLoadingAssignments } from "reduxes/courseService/assignment";
-import { AssignmentEntity } from "models/courseService/entity/AssignmentEntity";
 import { clearExamCreate } from "reduxes/coreService/questionCreate";
+import { clearSections, setLoadingSections, setSections } from "reduxes/courseService/section";
+import { CourseService } from "services/courseService/CourseService";
+import { ArrowDropDownIcon, ArrowRightIcon } from "@mui/x-date-pickers";
+import EditImageIcon from "@mui/icons-material/Edit";
+import SaveIcon from "@mui/icons-material/Save";
+import { ECourseResourceType } from "models/courseService/course";
+import { SectionService } from "services/courseService/SectionService";
+import { SectionEntity } from "models/courseService/entity/SectionEntity";
+import CloseIcon from "@mui/icons-material/Close";
+import Heading2 from "components/text/Heading2";
+import Heading5 from "components/text/Heading5";
+import EditSectionDialog from "./components/EditSectionDialog";
 
 const LecturerCourseAssignment = () => {
   const dispatch = useDispatch<AppDispatch>();
-  const examState = useSelector((state: RootState) => state.exam);
-  const assignmentState = useSelector((state: RootState) => state.assignment);
   const { courseId } = useParams<{ courseId: string }>();
-  const handleGetExams = useCallback(
-    async (id: string) => {
-      if (id === examState.courseId && examState.exams) {
-        return;
-      }
-      dispatch(setLoadingExams(true));
-      try {
-        const response = await ExamService.getExamsByCourseId(id);
-        dispatch(
-          setExams({
-            exams: response.exams,
-            courseId: id,
-            currentPage: response.currentPage,
-            totalItems: response.totalItems,
-            totalPages: response.totalPages
-          })
-        );
-      } catch (error) {
-        console.error(error);
-      }
-      dispatch(setLoadingExams(false));
-    },
-    [dispatch, examState.courseId, examState.exams]
-  );
+  const sectionState = useSelector((state: RootState) => state.section);
+  const [collapseOpen, setCollapseOpen] = useState<Array<Boolean>>([]);
+  const [isOpenEditTitle, setIsOpenEditTitle] = useState<Array<Boolean>>([]);
+  const [editSection, setEditSection] = useState<SectionEntity | null>(null);
 
-  const handleGetAssignments = useCallback(
-    async (id: string) => {
-      if (id === assignmentState.courseId && assignmentState.assignments.length > 0) {
-        return;
-      }
-      dispatch(setLoadingAssignments(true));
-      try {
-        const response = await AssignmentService.getAssignmentsByCourseId(id);
-        dispatch(
-          setAssignments({
-            assignments: response.assignments,
-            courseId: id
-          })
-        );
-      } catch (error) {
-        console.error(error);
-      }
-      dispatch(setLoadingAssignments(false));
-    },
-    [dispatch, assignmentState.courseId, assignmentState.assignments]
-  );
+  const handleGetSections = useCallback(async () => {
+    if (!courseId || (sectionState.courseId === courseId && sectionState.sections.length > 0)) {
+      return;
+    }
+
+    dispatch(setLoadingSections(true));
+    try {
+      const getSectionsResponse = await SectionService.getSectionsByCourseId(courseId);
+      dispatch(setSections({ sections: getSectionsResponse.sections, courseId: courseId }));
+    } catch (error) {
+      console.error("Failed to fetch sections", error);
+    }
+    dispatch(setLoadingSections(false));
+  }, [courseId, dispatch, sectionState.courseId, sectionState.sections]);
 
   useEffect(() => {
-    if (courseId) {
-      Promise.all([handleGetExams(courseId), handleGetAssignments(courseId)]);
-    }
-  }, [courseId, handleGetAssignments, handleGetExams]);
+    handleGetSections();
+  }, [courseId, handleGetSections]);
 
   const handleDeleteAssignment = useCallback(
     async (id: string) => {
       try {
         const response = await AssignmentService.deleteAssignment(id);
         if (response) {
-          const result = assignmentState.assignments.filter((e) => e.id !== id);
-          dispatch(setAssignments({ assignments: result }));
+          // const result = assignmentState.assignments.filter((e) => e.id !== id);
+          // dispatch(setAssignments({ assignments: result }));
+          dispatch(clearSections());
         }
       } catch (error) {
         console.error(error);
       }
     },
-    [assignmentState.assignments, dispatch]
+    [dispatch]
   );
 
   const handleDeleteExam = useCallback(
@@ -101,20 +78,19 @@ const LecturerCourseAssignment = () => {
       try {
         const response = await ExamService.deleteExam(id);
         if (response) {
-          const result = examState.exams.exams.filter((e) => e.id !== id);
-          dispatch(setExamList({ exams: result }));
+          dispatch(clearSections());
+          // const result = examState.exams.exams.filter((e) => e.id !== id);
+          // dispatch(setExamList({ exams: result }));
         }
       } catch (error) {
         console.error(error);
       }
     },
-    [examState.exams.exams, dispatch]
+    [dispatch]
   );
 
   const { t } = useTranslation();
-  const searchHandle = (searchVal: string) => {
-    console.log(searchVal);
-  };
+
   const navigate = useNavigate();
 
   const onCreateNewAssignment = (popupState: any) => {
@@ -127,7 +103,25 @@ const LecturerCourseAssignment = () => {
     if (courseId) navigate(routes.lecturer.exam.create.replace(":courseId", courseId));
     popupState.close();
   };
-  const [isReusedCourseResourceOpen, setIsReusedCourseResourceOpen] = useState(false);
+  const [isOpenEditSectionDialog, setOpenEditSectionDialog] = useState(false);
+
+  const handleCloseEditSectionDialog = () => {
+    setOpenEditSectionDialog(false);
+  };
+
+  const toggleItem = (index: number) => {
+    if (collapseOpen[index] === undefined)
+      setCollapseOpen((prevState: Array<Boolean>) => ({
+        ...prevState,
+        [index]: false
+      }));
+    else {
+      setCollapseOpen((prevState: any) => ({
+        ...prevState,
+        [index]: !Boolean(prevState[index])
+      }));
+    }
+  };
 
   // const onOpenReusedCourseResourceDialog = (popupState: any) => {
   //   setIsReusedCourseResourceOpen(true);
@@ -158,14 +152,14 @@ const LecturerCourseAssignment = () => {
   return (
     <>
       <Box className={classes.assignmentBody}>
-        <Heading1 translation-key='course_detail_assignment_list'>
-          {t("course_detail_assignment_list")}
-        </Heading1>
         <Grid container>
-          <Grid item xs={7}>
-            <SearchBar onSearchClick={searchHandle} />
+          <Grid item xs={3}>
+            <Heading1 translation-key='course_detail_assignment_list'>
+              {t("course_detail_assignment_list")}
+            </Heading1>
           </Grid>
-          <Grid item xs={5}>
+          <Grid item xs={1}></Grid>
+          <Grid item xs={8}>
             <MenuPopup
               popupId='add-question-popup'
               triggerButtonText={i18next.format(t("common_add_new"), "firstUppercase")}
@@ -197,72 +191,111 @@ const LecturerCourseAssignment = () => {
           </Grid>
         </Grid>
         <Box className={classes.assignmentsWrapper}>
-          <Box className={classes.topic}>
-            <Heading3 translation-key='course_detail_assignment'>
-              {t("course_detail_assignment")}
-            </Heading3>
-            {assignmentState.isLoading === true ? (
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  height: "100%",
-                  gap: "10px"
-                }}
-              >
-                <CircularProgress />
+          {sectionState.isLoading === false ? (
+            <Grid item xs={12}>
+              <Box margin={1} padding={0}>
+                <Grid container className={classes.gridBodyContainer}>
+                  <Grid item className={classes.topicWrapper} xs={12}>
+                    {sectionState.sections.map((topic, index) => {
+                      const isOpen =
+                        collapseOpen[index] === undefined ? true : Boolean(collapseOpen[index]);
+
+                      return (
+                        <Box className={classes.generalInfo} key={index}>
+                          <Box display='flex' alignItems='center' margin={1}>
+                            {isOpen ? (
+                              <IconButton
+                                className={classes.iconButtonActive}
+                                sx={{ padding: "5px" }}
+                                onClick={() => toggleItem(index)}
+                              >
+                                <ArrowDropDownIcon style={{ fontSize: 20 }} />
+                              </IconButton>
+                            ) : (
+                              <IconButton
+                                className={classes.iconButton}
+                                sx={{ padding: "5px" }}
+                                onClick={() => toggleItem(index)}
+                              >
+                                <ArrowRightIcon style={{ fontSize: 20 }} />
+                              </IconButton>
+                            )}
+                            {isOpenEditTitle[index] === undefined || isOpenEditTitle[index] ? (
+                              <Heading5>{topic.name}</Heading5>
+                            ) : (
+                              <TextField variant='standard' defaultValue={topic.name} />
+                            )}
+                            <Box>
+                              {isOpenEditTitle[index] || isOpenEditTitle[index] === undefined ? (
+                                <IconButton
+                                  onClick={() => {
+                                    setOpenEditSectionDialog(true);
+                                    setEditSection(topic);
+                                  }}
+                                  className={classes.editTopicTitleImageContainer}
+                                >
+                                  <EditImageIcon />
+                                </IconButton>
+                              ) : (
+                                <></>
+                              )}
+                            </Box>
+                          </Box>
+                          <Collapse
+                            in={isOpen}
+                            timeout='auto'
+                            unmountOnExit
+                            className={classes.collapse}
+                          >
+                            {topic.modules.map((resource, resourceIndex) =>
+                              resource.typeModule === ECourseResourceType.assignment ? (
+                                <AssignmentResource
+                                  key={resource.moduleId}
+                                  courseId={courseId}
+                                  examId={resource.assignment?.id}
+                                  resourceTitle={resource.assignment?.title}
+                                  resourceOpenDate={resource.assignment?.timeOpen}
+                                  resourceEndedDate={resource.assignment?.timeClose}
+                                  intro={resource.assignment?.intro}
+                                  type={ResourceType.assignment}
+                                  onDelete={handleDeleteAssignment}
+                                />
+                              ) : (
+                                <AssignmentResource
+                                  key={resource.moduleId}
+                                  courseId={courseId}
+                                  examId={resource.exam?.id}
+                                  resourceTitle={resource.exam?.name}
+                                  resourceOpenDate={resource.exam?.timeOpen}
+                                  resourceEndedDate={resource.exam?.timeClose}
+                                  intro={resource.exam?.intro}
+                                  type={ResourceType.exam}
+                                  onDelete={handleDeleteExam}
+                                />
+                              )
+                            )}
+                          </Collapse>
+                        </Box>
+                      );
+                    })}
+                  </Grid>
+                </Grid>
               </Box>
-            ) : (
-              assignmentState.assignments.length > 0 &&
-              assignmentState.assignments.map((assignment: AssignmentEntity) => (
-                <AssignmentResource
-                  key={assignment.id}
-                  courseId={courseId}
-                  examId={assignment.id}
-                  resourceTitle={assignment.title}
-                  resourceOpenDate={assignment.timeOpen}
-                  resourceEndedDate={assignment.timeClose}
-                  intro={assignment.intro}
-                  type={ResourceType.assignment}
-                  onDelete={handleDeleteAssignment}
-                />
-              ))
-            )}
-          </Box>
-          <Box className={classes.topic}>
-            <Heading3 translation-key='course_detail_exam'>{t("course_detail_exam")}</Heading3>
-            {examState.isLoading === true ? (
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  height: "100%",
-                  gap: "10px"
-                }}
-              >
-                <CircularProgress />
-              </Box>
-            ) : (
-              examState.exams.exams.length > 0 &&
-              examState.exams.exams.map((exam) => (
-                <AssignmentResource
-                  key={exam.id}
-                  courseId={courseId}
-                  examId={exam.id}
-                  resourceTitle={exam.name}
-                  resourceOpenDate={exam.timeOpen}
-                  resourceEndedDate={exam.timeClose}
-                  intro={exam.intro}
-                  type={ResourceType.exam}
-                  onDelete={handleDeleteExam}
-                />
-              ))
-            )}
-          </Box>
+            </Grid>
+          ) : (
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "100%",
+                gap: "10px"
+              }}
+            >
+              <CircularProgress />
+            </Box>
+          )}
         </Box>
       </Box>
       {/* <ReusedCourseResourceDialog
@@ -280,6 +313,11 @@ const LecturerCourseAssignment = () => {
         handleClose={onCloseReusedResourceDialog}
         translation-key={["course_lecturer_resource_list", "common_back"]}
       /> */}
+      <EditSectionDialog
+        section={editSection}
+        open={isOpenEditSectionDialog}
+        onClose={handleCloseEditSectionDialog}
+      />
     </>
   );
 };
